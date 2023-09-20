22f8cb3f9dc9992d5e40777d5d7bb4a896cd189c
Joshua Colvin <joshua@joshuacolvin.net>
2023-09-20 19:50:01 -0400

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  NgZone,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class AppComponent {
-  title = 'clarity-example';
+  constructor(private ngZone: NgZone) {}
+
+  @ViewChild("email") set email(element: ElementRef) {
+    if (element) {
+      this.focusElement(element);
+    }
+  }
+
+  @ViewChild("phone") set phone(element: ElementRef) {
+    if (element) {
+      this.focusElement(element);
+    }
+  }
+
+  @ViewChild("firstName") set firstName(element: ElementRef) {
+    if (element) {
+      this.focusElement(element);
+    }
+  }
+
+  focusElement(element: ElementRef): void {
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => element.nativeElement.focus());
+    });
+  }
 }
